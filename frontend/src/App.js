d7847482596ce53708ee8094424ffc4ad37977b6
Tharindu Manoj Thilakarathna <114139836+manojtharindu11@users.noreply.github.com>
2023-09-17 16:28:43 +0530

    Revert "finished with copy of same code segment"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import Navigation from './navigation/Nav';
-import Products from './products/Products';
-import Recommended from './recommended/Recommended';
-import Sidebar from './sidebar/Sidebar';
 
 function App() {
   return (
-    <>
-      <Sidebar />
-      <Navigation />
-      <Recommended />
-      <Products />
-    </>
+    <div className="App">
+      hello world
+    </div>
   );
 }
 
